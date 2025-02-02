5a4eacc5756eff9102900349147b7544b77daea2
seomac <icyveins@hotmail.com>
2025-02-02 18:16:32 +0800

    add axis to plotclient and test

--- possh/plotclient.py
+++ possh/plotclient.py
@@ -22,11 +22,21 @@ def show(*args, **kwargs):
         )
     )
 
+def axis(*args, **kwargs):
+    _client.connectAndSend(
+        _client.serializePlotInstructions(
+            'pyplot.axis', *args, **kwargs
+        )
+    )
+
 # ========== Dev testing
 if __name__ == '__main__':
     import numpy as np
     figure()
     plot((1,2,3), (4,5,1))
+
+    figure()
     plot(np.arange(100)*0.01, np.sin(np.arange(100)*0.01), 'rx-')
+    axis([0, 2, -1, 1])
     show()
 
