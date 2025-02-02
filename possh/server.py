84dc7228ff0ca5da8847389b04baed6a2d68bbe6
seomac <icyveins@hotmail.com>
2025-02-02 16:43:52 +0800

    loop over listen/accept() in server so we can keep receiving

--- possh/server.py
+++ possh/server.py
@@ -10,15 +10,18 @@ class Server:
         # Begin a simple TCP server
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind(('', self._port))
-            s.listen()
-            conn, addr = s.accept()
-            with conn:
-                print('Connected by', addr)
-                while True:
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    conn.sendall(data)
+            # Let's just loop after every connection to get the next one for now
+            while True:
+                print("Waiting for a connection...")
+                s.listen()
+                conn, addr = s.accept()
+                with conn:
+                    print('Connected by', addr)
+                    while True:
+                        data = conn.recv(1024)
+                        if not data:
+                            break
+                        conn.sendall(data)
 
 # ============= Developmental testing
 if __name__ == '__main__':
